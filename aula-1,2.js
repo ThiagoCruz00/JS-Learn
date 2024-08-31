f6d5f6d9d5e1db7a0d29b582c7d967215855f180
ThiagoCruz00 <thiagohfs.cruz@gmail.com>
2024-08-31 13:49:22 -0300

    feat: Rename

---
+++ aula-1,2.js
@@ -0,0 +1,47 @@
+//Tipos de variaveis
+
+var a = "chocolate"    //Variável que varia
+let b = "laranja"      //Outra variável que varia
+const c = 10           //Não pode receber um novo valor
+
+
+// Tipos de dados:
+
+// String
+const name = "Meu nome"
+
+// Number
+const num = 1
+
+// Boolean
+const isApproved = false
+
+//nulo
+const lastName = null
+
+
+// Undefined
+let address
+
+// Object
+const brand = {
+    name: "nike",
+    size: ["P", "M", "G"]
+}
+
+// Array
+const languages = [
+    "java",
+    "javascript"
+]
+
+
+//Tipos de consoles
+
+console.log()
+console.warn()
+console.error()
+console.table()
+
+// limpar consoles após a visualização//
+// console.clear("Limpei")
